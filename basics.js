352336dbea521789cb4b12cb2eb77bf47cb8b5b1
Martin Ottesen <martinottesen@Martins-MacBook-Pro-2.local>
2019-08-30 12:23:17 +0200

    substring own name

--- basics.js
+++ basics.js
@@ -1,1 +1,6 @@
-console.log("hej");
+const name = "Peter";
+const pet = "cat";
+const petName = "Mandu";
+
+console.log(`My name is Peter ${name}.
+I have a ${pet} called ${petName}`);
---
+++ nameparts.js
@@ -0,0 +1,9 @@
+const name = "Martin Berg Ottesen";
+
+const firstName = name.substring(0, 6);
+const middleName = name.substring(7, 11);
+const lastName = name.substring(12, 20);
+
+console.log(firstName);
+console.log(middleName);
+console.log(lastName);
